b44701883b3a472150a493c17cf5176baa26342e
mumu111111 <2646709035@qq.com>
2018-02-27 14:59:07 +0800

    版本二

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
 
-let n=1
-setInterval( ()=>{
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-        .one('transitionend', (e)=>{
-            console.log()
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+let n
+init()
+
+setInterval(() => {
+    makeLeave(getImg(n))
+        .one('transitionend', (e) => {
+            makeEnter($(e.currentTarget))
         })
-
-    $(`.images > img:nth-child(${x(n+1)}`).removeClass('enter').addClass('current')
+    makeCurrent(getImg(n+1))
     n += 1
-},2000)
+}, 2000)
 
-function x(n){//1 2 3 4 5 6 7 8 9
-    if(n>3){
-        n= n%3
-        if(n===0){ //6%3===0
-            n=3
+
+
+function x(n) {//1 2 3 4 5 6 7 8 9.....
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) { //6%3===0
+            n = 3
         }
     }// 1 2 3
     return n
+}
+function init() {
+    n = 1
+    $(`.images > img:nth-child(${x(n)})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+    return  $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    return $node.removeClass('current enter').addClass('leave')
+}
+
+function makeEnter($node){
+    return  $node.removeClass('leave current').addClass('enter') 
+}
+
+function getImg(n) {
+    return $(`.images > img:nth-child(${x(n)})`) //有变量n
 }
